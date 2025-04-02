61b1f98c1bb2cbfde652aef75b24e340ff064dfb
Raimon Esteve <raimonesteve@gmail.com>
2025-04-02 09:22:05 +0200

    Initial commit

---
+++ tests/test_module.py
@@ -0,0 +1,11 @@
+# This file is part discount_base_price module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class DiscountBasePriceTestCase(ModuleTestCase):
+    'Test Discount Base Price module'
+    module = 'discount_base_price'
+
+del ModuleTestCase
