e718164f045aa4c6db33e5ea83f734a6fb49f430
Raimon Esteve <raimonesteve@gmail.com>
2025-11-04 11:20:19 +0100

    Set unit_price from base_price (account invoice) #182014

--- __init__.py
+++ __init__.py
@@ -2,9 +2,14 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
+from . import invoice
 from . import sale
 
 def register():
+    Pool.register(
+        invoice.InvoiceLine,
+        depends=['account_invoice_discount'],
+        module='discount_base_price', type_='model')
     Pool.register(
         sale.SaleLine,
         depends=['sale_discount'],
